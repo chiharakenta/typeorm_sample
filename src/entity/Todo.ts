591a1076344501d88bf4325455d11c4137f62c13
chiharakenta <chiharakenta0519@gmail.com>
2021-01-28 14:14:26 +0900

    feat: 🎸 create todos table

---
+++ src/entity/Todo.ts
@@ -0,0 +1,12 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Todo {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  content: string;
+
+}
